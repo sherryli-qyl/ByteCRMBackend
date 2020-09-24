1bc0eceac5ec2531a9a311e7a3d3131054f62767
cathy yang <cathy.yf.yang@gmail.com>
2020-09-24 17:14:51 +1000

    version2
    version2

---
+++ src/routes/companies.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {addCompany, 
+       getCompany, 
+       getAllCompanies, 
+       updateCompany, 
+       deleteCompany,
+       addContact,
+       removeContact
+    } = require('../controllers/Companies');//导入功能
+
+router.get('/', getAllCompanies);
+router.get('/:code', getCompany);
+router.post('/', addCompany);
+router.put('/:code',updateCompany);
+router.delete('/:code', deleteCompany); 
+
+router.post('/:code/contacts/:id', addContact);
+router.post('/:code/contacts/:id', removeContact);
+
+
+module.exports = router;
