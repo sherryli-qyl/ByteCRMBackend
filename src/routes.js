01f3a57ede33c03c8b5366c4f4c2577b5202c893
DerrickSong <sbz7drk@gmail.com>
2020-09-24 15:38:23 +1000

    add files about user login and register

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,13 @@ const contactRoute = require('./routes/contacts.js');
 
 const companyRoute = require('./routes/companies.js');
 
+const userRoute = require('./routes/users');
 
-router.use('/contacts',contactRoute);
+const authRoute = require('./routes/auth');
+
+router.use('/contacts', contactRoute);
 router.use('/companies', companyRoute);
+router.use('/users', userRoute);
+router.use('/auth', authRoute);
 
 module.exports = router;
