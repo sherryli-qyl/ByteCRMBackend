e5bc3bc40b1534188222dbaa4b7edb3194fa3316
Bradley <liu770807152@hotmail.com>
2020-10-03 22:54:00 +1000

    developing controller

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,48 +1,71 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const testPhoneNum = require("../utils/testPhoneNum");
 
-const schema = new mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            required: true
-        },
-        lastName: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            validate: {
-                validator: (email) => {
-                    // const validation = Joi.string().email().validate(email);
-                    // const error = validation.error;
-                    // // return false代表validator验证失败
-                    // if (error) {
-                    //   return false;
-                    // } else {
-                    //   return true;
-                    // }
+const schema = new mongoose.Schema({
+  _id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (email) => {
+        // 如果error有值，则校验失败
+        return !Joi.string().email().validate(email).error;
+      },
+      msg: "Invalid email format",
+    },
+  },
+  phoneNumber: {
+    type: String,
+    required: false,
+    validate: {
+      validator: (phoneNum) => {
+        return testPhoneNum(phoneNum);
+      },
+      msg: "Invalid phone number format",
+    },
+  },
+  contactOwner: {
+    type: String,
+    required: false,
+  },
+  lastActivityDate: {
+    type: Date,
+    required: false,
+  },
+  leadStatus: {
+    type: String,
+    required: false,
+  },
+  companies: [{ type: String, ref: "Company" }],
+  __v: {
+    type: Number,
+    select: false,
+  },
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    timestamps: true,
+    firstName: false,
+    lastName: false,
+});
 
-                    // 如果error有值，则校验失败
-                    return !Joi.string().email().validate(email).error;
-                },
-                msg: 'Invalid email format'
-            }
-        },
-        companies:
-            //{type: 
-            [{ type: String, ref: 'Company' }],
-        //select: false
-        //default:[]
-        //},
-        __v: {
-            type: Number,
-            select: false
-        }
-    }
-);
-
-const model = mongoose.model('Contact', schema);
-module.exports = model; 
+schema.virtual('name').get(function () {
+    return this.firstName + ' ' + this.lastName;
+})
+const model = mongoose.model("ContactModel", schema);
+module.exports = model;
