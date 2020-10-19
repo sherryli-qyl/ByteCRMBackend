aed14149ea8893f5ad9cadea81c06f267c0a0637
yyr19921203 <123775172@qq.com>
2020-10-20 02:52:25 +1100

    merge master

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -15,9 +15,7 @@ async function loginUser(req, res) {
   }
 
   const token = generateToken(existingUser._id);
-  console.log(res.setHeader('x-auth-token',token));
-  // return res.status(204).json({ email, token });
-  return res.set('x-auth-token',token).json({ email, token });
+  return res.setHeader('x-auth-token').json({ email, token });
 }
 
 module.exports = { 
